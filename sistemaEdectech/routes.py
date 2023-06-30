aa2b3cd3d89c375b563d75e992e13c2a233fc7f2
Henrique <henrique.coolcat15@gmail.com>
2023-06-30 18:55:33 -0300

    demo da aplicacao

--- sistemaEdectech/routes.py
+++ sistemaEdectech/routes.py
@@ -13,6 +13,37 @@ def home():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form_login = FormLogin()
+    if form_login.validate_on_submit() and 'botao_login' in request.form:
+        usuario = User.query.filter_by(email=form_login.email.data).first()
+        if usuario and bcrypt.check_password_hash(usuario.senha, form_login.senha.data):
+            login_user(usuario, remember=form_login.lembrar_dados.data)
+            flash('Login feito com Sucesso', 'alert-success')
+            par_next = request.args.get('next')
+            if par_next:
+                return redirect(par_next)
+            else:
+                return redirect(url_for('perfil'))
+        else:
+            flash('falha no login, Email ou senha errado', 'alert-danger')
+    return render_template('login.html', form_login=form_login)
+
+
+@app.route('/cadastro', methods=['GET', 'POST'])
+def cadastro():
+    form_criarconta = FormCriarConta()
+    if form_criarconta.validate_on_submit() and 'botao_criarconta' in request.form:
+        senha_cript = bcrypt.generate_password_hash(form_criarconta.senha.data)
+        usuario = User(nome=form_criarconta.username.data, email=form_criarconta.email.data, senha=senha_cript)
+        db.session.add(usuario)
+        db.session.commit()
+        flash('Conta Criada com Sucesso', 'alert-success')
+        return redirect(url_for('perfil'))
+    return render_template('cadastro.html', form_criarconta=form_criarconta)
+
+
+""""@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form_login = FormLogin()
     form_criarconta = FormCriarConta()
     if form_login.validate_on_submit() and 'botao_login' in request.form:
         usuario = User.query.filter_by(email=form_login.email.data).first()
@@ -34,6 +65,7 @@ def login():
         flash(f'Conta Criada com Sucesso', 'alert-success')
         return redirect(url_for('perfil'))
     return render_template('login.html', form_login=form_login, form_criarconta=form_criarconta)
+"""
 
 
 @app.route('/sair')
@@ -55,3 +87,14 @@ def perfil():
 def usuarios():
     usuario = User.query.order_by(User.id.desc())
     return render_template('usuarios.html', usuario=usuario)
+
+
+@app.route('/carreiras')
+def carreiras():
+    return render_template('carreiras.html')
+
+
+@app.route('/desenvolvedores')
+def desenvolvedores():
+    return render_template('desenvolvedores.html')
+
